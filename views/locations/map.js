552099d477fb94b96bde211c4594c352a89a3a03
John Mertens <john.mertens@gmail.com>
2011-06-26 10:17:00 +0800

    Add the doc ID to the locations couch view

--- views/locations/map.js
+++ views/locations/map.js
@@ -1,5 +1,5 @@
 function(doc) {
     if(doc.geo) {
-        emit(doc._id, {lat: doc.geo.coordinates[0], lon: doc.geo.coordinates[1], text: doc.text, tweet_image: doc.tweet_image, thumbnail: doc.user.profile_image_url, name: doc.user.screen_name});
+        emit(doc._id, {id: doc._id, lat: doc.geo.coordinates[0], lon: doc.geo.coordinates[1], text: doc.text, tweet_image: doc.tweet_image, thumbnail: doc.user.profile_image_url, name: doc.user.screen_name});
     }
 }
