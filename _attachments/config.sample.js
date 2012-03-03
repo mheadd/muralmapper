b4d6dfa60b40e491e785895c668e0e1aacc82d76
Todd Willey <xtoddx@gmail.com>
2012-03-03 17:36:53 -0500

    Clean up repo where sensitive info could be.

--- _attachments/config.sample.js
+++ _attachments/config.sample.js
@@ -1,8 +1,8 @@
 // downtown lexington at center_x and center_y
-
 var MapConfig = {
+	"page_title": "Lexington Public Art Mapper",
 	"twitter_id": "lexpubart",
 	"center_x": 38.04616,
 	"center_y": -84.497089,
-	"gmaps_api_key": "AIzaSyCJO7Y3igbon0wH1CsL9UfQSR1EhUqaZZ4"
+	"gmaps_api_key": "your-key-here"
 }
