334384827ea59677696c0910353b4620822d1c3d
Chase Southard <chase.southard@gmail.com>
2012-03-03 16:51:35 -0500

    adding values to config. adding title and keys to index

--- _attachments/config.js
+++ _attachments/config.js
@@ -3,5 +3,6 @@
 var MapConfig = {
 	"twitter_id": "lexpubart",
 	"center_x": 38.04616,
-	"center_y": -84.497089
+	"center_y": -84.497089,
+	"gmaps_api_key": "AIzaSyCJO7Y3igbon0wH1CsL9UfQSR1EhUqaZZ4"
 }
